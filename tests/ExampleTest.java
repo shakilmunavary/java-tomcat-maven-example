13e0e0c5eb368e8e209fe8442ec0acb7a6db2557
shakilmunavary <shakil.ahamed@gmail.com>
2025-03-21 11:27:16 +0530

    Add generated test cases

---
+++ tests/ExampleTest.java
@@ -0,0 +1,11 @@
+
+    import org.junit.jupiter.api.Test;
+    import static org.junit.jupiter.api.Assertions.*;
+
+    public class ExampleTest {
+        @Test
+        public void testExample() {
+            assertEquals(1, 1);
+        }
+    }
+    
